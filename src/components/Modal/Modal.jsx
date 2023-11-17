ef573b0b4f9c59856dcbfffcce128716e2538f75
VIPon4ik <umniyd@gmail.com>
2023-11-17 18:25:58 +0200

    Refactored Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,33 +1,32 @@
-import React, { Component } from 'react';
 import { createPortal } from 'react-dom';
 import { ModalContainer, Overlay } from './Modal.styled';
+import { useEffect } from 'react';
 
 const modalRoot = document.getElementById('modal-root');
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleExit)
-  }
+export const Modal = ({ onEsc, largeImage }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleExit);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleExit);
-  }
+    return () => {
+      window.removeEventListener('keydown', handleExit);
+    };
+  }, []);
 
-  handleExit = e => {
+  const handleExit = e => {
     if (e.code === 'Escape') {
-      this.props.onEsc();
+      onEsc();
     }
-  }
+  };
 
-  render() {
-    const { largeImage } = this.props;
-    return createPortal(
-      <Overlay>
-        <ModalContainer>
-          <img src={largeImage} alt=''/>
-        </ModalContainer>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <Overlay>
+      <ModalContainer>
+        <img src={largeImage} alt="" />
+      </ModalContainer>
+    </Overlay>,
+    modalRoot
+  );
+};
+
+export default Modal;
