f5fb123e010d72e92deb0d9f470bda9d5a12861c
VIPon4ik <umniyd@gmail.com>
2023-11-17 18:31:30 +0200

    Refactored ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,24 +1,26 @@
-import React, { Component } from 'react'
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 import { GalleryItem, GalleryImage } from './ImageGalleryItem.styled';
 import Modal from 'components/Modal/Modal';
 
-export default class ImageGalleryItem extends Component {
-  state = {
-    show: false,
-  }
+export const ImageGalleryItem = ({ smallImage, largeImage }) => {
+  const [show, setShow] = useState(false);
 
-  showModal = e => {
-    this.setState(prevState => ({ show: !prevState.show }));
-  }
+  const showModal = e => {
+    setShow(prevState => !prevState.show);
+  };
 
-  render() {
-    const { smallImage, largeImage } = this.props;
-    const { show } = this.state;
-    return (
-      <GalleryItem onClick={this.showModal}>
-        <GalleryImage src={smallImage} alt='Query image'/>
-        {show && <Modal largeImage={largeImage} onEsc={this.showModal}/>}
-      </GalleryItem>
-    )
-  }
-}
+  return (
+    <GalleryItem onClick={showModal}>
+      <GalleryImage src={smallImage} alt="Query image" />
+      {show && <Modal largeImage={largeImage} onEsc={showModal} />}
+    </GalleryItem>
+  );
+};
+
+ImageGalleryItem.propTypes = {
+  smallImage: PropTypes.string.isRequired,
+  largeImage: PropTypes.string.isRequired,
+};
+
+export default ImageGalleryItem;
